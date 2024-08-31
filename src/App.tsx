17608fa40fcea5047f01eab9600a578a1b6bd828
Kotaro <kotaro@KotaronoMacBook-Air.local>
2024-08-31 21:29:56 +0900

    stylelint追加

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,14 @@ import logo from '@/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="app">
+      <header className="appHeader">
+        <img src={logo} className="appLogo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="appLink"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
